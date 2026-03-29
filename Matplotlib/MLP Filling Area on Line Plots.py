bd8c72c64c063173f18d1bb6c574855b8428eda6
ahmad <ahmadaljmal254@gmail.com>
2026-03-29 19:31:28 +0300

    __ MPL Filling Area on Line Plots __

---
+++ Matplotlib/MLP Filling Area on Line Plots.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use('fivethirtyeight')
+data = pd.read_csv('data (1).csv')
+ages = data['Age']
+dev_salaries = data['All_Devs']
+py_salaries = data['Python']
+js_salaries = data['JavaScript']
+plt.plot(ages, dev_salaries,
+         linestyle='--', label='All Devs')
+plt.plot(ages, py_salaries, label='Python')
+overall_median = 57287
+plt.fill_between(ages, py_salaries, dev_salaries,
+                 where=(py_salaries > dev_salaries),
+                 interpolate=True, alpha=0.25, label='Above Avg')
+plt.fill_between(ages, py_salaries, dev_salaries,
+                 where=(py_salaries <= dev_salaries),
+                 interpolate=True, alpha=0.25, label='Below Avg')
+plt.legend()
+plt.title('Median Salary (USD) by Age')
+plt.xlabel('Ages')
+plt.ylabel('Median Salary (USD)')
+plt.tight_layout()
+plt.show()
