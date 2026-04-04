157d4cf8d3cc7a513dcde9ad59406bdc1d687db8
ahmad <ahmadaljmal254@gmail.com>
2026-04-05 00:54:07 +0300

    __ Check Boxes __

--- customtkinter/Check Boxes.py
+++ customtkinter/Check Boxes.py
@@ -56,3 +56,25 @@ clear_button.pack(pady=10)
 my_label.pack(pady=10)
 
 alpha.mainloop()
+# 1. Special Variables (Tkinter Variables)
+# customtkinter.StringVar(value="off"): A library-specific text
+# variable definition that monitors the state of an element instantaneously.
+# check_var: Used to store the "state" of the box (is it specified or not).
+# text_var: Used to dynamically change the "text" written next to the box
+# while the program is running
+# 2. CTkCheckBox (Checkbox) Properties
+# variable=check_var: Bind the box to a state variable so the program
+# knows if it is activated or not.
+# onvalue="on" and offvalue="off": Specifies the values the check_var variable will
+# take when activated or deactivated (you can change them to 1 and 0, for example).
+# textvariable=text_var: Bind the visible text to the variable text_var;
+# any change to this variable will immediately change the text on the screen
+# checkbox_width and checkbox_height: Control the size of the small checkbox for selection.
+# corner_radius=50: Make the square appear completely circular
+# 3. Control functions (Methods)
+# my_check.select(): A program command to activate the box (check it) automatically.
+# my_check.deselect(): A command to programmatically deactivate the box.
+# my_check.toggle(): An intelligent function that reverses the state; If it is activated,
+# you cancel it, and if it is canceled, you do it.
+# check_var.get(): Fetch the current value of the box (on or off) to process it in functions.
+# text_var.set("..."): Update the text associated with the box immediately.
